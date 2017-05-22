22f2a04460e3e1279f34bb208cdc8d9fc25defb8
pawel.wiklowski <pawel.wiklowski@gmail.com>
2017-05-22 21:39:55 +0200

    Add basic protocol message handling

--- app/app.ino
+++ app/app.ino
@@ -1,9 +1,13 @@
+
+#include <WebSocketsClient.h>
+#include <WebSockets.h>
 #include <ESP8266WiFi.h>
 
 const char* ssid = "ASUS";
 const char* password = "pass";
 const char* espname = "Test device";  //Shows in router and in OTA-menu
 
+WebSocketsClient webSocket;
 void setup() {
   Serial.begin(115200);
   WiFi.hostname(espname);
@@ -13,9 +17,71 @@ void setup() {
     delay(500);
   }
   Serial.print("Connected!\n");
+  webSocket.begin("192.168.1.239", 12345, "/connect");
+  webSocket.onEvent(webSocketEvent);
 }
 
+void parseMessage(uint8_t* payload, size_t len){
+  StaticJsonBuffer<500> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.parseObject(payload);
+
+  String eventName = root["name"];
+  //Serial.print("Event %s\n", eventName);
+
+//TODO: use ESP.getChipId() to set id
+  
+  if (eventName == "RequestGetDevices"){
+    Serial.print("devices requested!\n");
+     webSocket.sendTXT("{\n"
+"    \"name\": \"EventDeviceListUpdate\",\n"
+"    \"payload\": {\n"
+"        \"devices\": [\n"
+"            {\n"
+"                \"id\": \"0685B960-736F-46F7-BEC0-9E6CBD61ADC1\",\n"
+"                \"name\": \"ESP Light \",\n"
+"                \"variables\": [\n"
+"                    {\n"
+"                        \"href\": \"/switch\",\n"
+"                        \"if\": \"oic.if.rw\",\n"
+"                        \"n\": \"Switch\",\n"
+"                        \"rt\": \"oic.r.switch.binary\",\n"
+"                        \"values\": {\n"
+"                            \"rt\": \"oic.r.switch.binary\",\n"
+"                            \"value\": 1\n"
+"                        }\n"
+"                    }\n"
+"                ]\n"
+"            }\n"
+"]}}");
+  }else if(eventName =="RequestSetValue"){
+    bool val = root["payload"]["value"]["value"];
+     Serial.printf("set value %d!\n", (int)val);
+  }
+  
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n",  payload);
+      webSocket.sendTXT("{\"name\":\"RequestAuthorize\", \"payload\": { \"token\": \""+TOKEN+"\", \"name\": \"ESP8266 Demo\", \"uuid\": \"UUID\"}}}");
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+
+      parseMessage(payload, length);
+      
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+      break;
+  }
+}
 void loop() {
-  Serial.print("HI!\n");
-  delay(1000);
+   webSocket.loop(); 
 }
